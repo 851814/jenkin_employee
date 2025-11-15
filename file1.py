7a94de5b13f1dd754c864584fd6aad271c0e999f
soumya-sunkad <soumyasunkad06@gmail.com>
2025-11-15 12:04:58 +0530

    Create file1.py with employee information
    Add employee details and print statements

---
+++ file1.py
@@ -0,0 +1,9 @@
+name="soumya"
+emp_id="101"
+salary=80000
+address="Hubli"
+print("Employee Details")
+print("Employee Name:",name)
+print("Employee Id:",emp_id)
+print("Salary:",salary)
+print("Address:"address)
